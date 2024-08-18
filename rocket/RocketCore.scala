06877f9121afd89caf6a8e6269aaa9b81e4d783c
Derain123 <andeyou2021@163.com>
2024-08-18 16:40:49 +0800

    runaheadv2 config

--- rocket/RocketCore.scala
+++ rocket/RocketCore.scala
@@ -1674,6 +1674,17 @@ when(runahead_posedge) {
   }
   else {
     when (csr.io.trace(0).valid) {
+      midas.targetutils.SynthesizePrintf(printf("C%d: %d [%d] pc=[%x] W[r%d=%x][%d] R[r%d=%x] R[r%d=%x] inst=[%x] DASM(%x)\n",
+         io.hartid, coreMonitorBundle.timer, coreMonitorBundle.valid,
+         coreMonitorBundle.pc,
+         Mux(wb_ctrl.wxd || wb_ctrl.wfd, coreMonitorBundle.wrdst, 0.U),
+         Mux(coreMonitorBundle.wrenx, coreMonitorBundle.wrdata, 0.U),
+         coreMonitorBundle.wrenx,
+         Mux(wb_ctrl.rxs1 || wb_ctrl.rfs1, coreMonitorBundle.rd0src, 0.U),
+         Mux(wb_ctrl.rxs1 || wb_ctrl.rfs1, coreMonitorBundle.rd0val, 0.U),
+         Mux(wb_ctrl.rxs2 || wb_ctrl.rfs2, coreMonitorBundle.rd1src, 0.U),
+         Mux(wb_ctrl.rxs2 || wb_ctrl.rfs2, coreMonitorBundle.rd1val, 0.U),
+         coreMonitorBundle.inst, coreMonitorBundle.inst))
       printf("C%d: %d [%d] pc=[%x] W[r%d=%x][%d] R[r%d=%x] R[r%d=%x] inst=[%x] DASM(%x)\n",
          io.hartid, coreMonitorBundle.timer, coreMonitorBundle.valid,
          coreMonitorBundle.pc,
